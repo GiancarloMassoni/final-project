af5ac228584012599ccf462c533a1091b3520436
Giancarlo Massoni <massonicarlo55@gmail.com>
2023-01-23 22:43:57 +0000

    Added sign in and sign up

--- server/index.js
+++ server/index.js
@@ -5,9 +5,9 @@ const errorMiddleware = require('./error-middleware');
 const jsonMiddleware = express.json();
 const pg = require('pg');
 const path = require('path');
-// const jwt = require('jsonwebtoken');
-// const argon2 = require('argon2');
-// const ClientError = require('./client-error');
+const jwt = require('jsonwebtoken');
+const argon2 = require('argon2');
+const ClientError = require('./client-error');
 const db = new pg.Pool({
   connectionString: 'postgres://dev:dev@localhost/healthyHacks',
   ssl: {
@@ -20,7 +20,6 @@ const publicPath = path.join(__dirname, 'public');
 
 app.use(jsonMiddleware);
 app.use(staticMiddleware);
-app.use(express.static(publicPath));
 
 app.post('/api/restaurants', (req, res) => {
   const sql = `
@@ -113,6 +112,65 @@ app.delete('/api/meals/:meal', (req, res) => {
       console.error(error);
       res.status(500).json({ error: 'An unexpected error occured.' });
     });
+});
+
+app.use(express.static(publicPath));
+app.use(express.json());
+
+app.post('/api/auth/sign-up', (req, res, next) => {
+  const { username, password } = req.body;
+  if (!username || !password) {
+    throw new ClientError(400, 'username and password are required fields');
+  }
+  argon2
+    .hash(password)
+    .then(hashedPassword => {
+      const sql = `
+        insert into "users" ("username", "hashedPassword")
+        values ($1, $2)
+        returning "userId", "username"
+      `;
+      const params = [username, hashedPassword];
+      return db.query(sql, params);
+    })
+    .then(result => {
+      const [user] = result.rows;
+      res.status(201).json(user);
+    })
+    .catch(err => next(err));
+});
+
+app.post('/api/auth/sign-in', (req, res, next) => {
+  const { username, password } = req.body;
+  if (!username || !password) {
+    throw new ClientError(401, 'invalid login');
+  }
+  const sql = `
+    select "userId",
+           "hashedPassword"
+      from "users"
+     where "username" = $1
+  `;
+  const params = [username];
+  db.query(sql, params)
+    .then(result => {
+      const [user] = result.rows;
+      if (!user) {
+        throw new ClientError(401, 'invalid login');
+      }
+      const { userId, hashedPassword } = user;
+      return argon2
+        .verify(hashedPassword, password)
+        .then(isMatching => {
+          if (!isMatching) {
+            throw new ClientError(401, 'invalid login');
+          }
+          const payload = { userId, username };
+          const token = jwt.sign(payload, process.env.TOKEN_SECRET);
+          res.json({ token, user: payload });
+        });
+    })
+    .catch(err => next(err));
 });
 
 app.use(errorMiddleware);
