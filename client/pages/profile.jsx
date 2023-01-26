7cd9c87bae9965012694f057b1f464b0bfb5a81c
Giancarlo Massoni <massonicarlo55@gmail.com>
2023-01-26 03:24:42 +0000

    Added fetch for restaurants

---
+++ client/pages/profile.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import AppContext from '../lib/app-context';
+
+export default class Profile extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      restuarants: null,
+      meals: []
+    };
+  }
+
+  componentDidMount() {
+    const { user } = this.context;
+    fetch(`/api/restaurants/${user.userId}`, {
+      method: 'GET',
+      headers: {
+        'Content Type': 'application/json'
+      }
+    }
+      .then(res => res.json())
+      .then(data => {
+        this.setState({ restuarants: data });
+      })
+      // eslint-disable-next-line no-console
+      .catch(err => console.log('Fetch Get error:', err)));
+  }
+
+  render() {
+    return (
+      <p>{this.state.restuarants}</p>
+    );
+  }
+}
+
+Profile.contextType = AppContext;
